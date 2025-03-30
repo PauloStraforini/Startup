6347cbc8d8b1f67073290656645a9a0600809551
PauloStraforini <paulostraforini@gmail.com>
2025-03-30 20:15:55 -0300

    ❌  Delete

--- prisma/migrations/20250322132736_v5_0/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `senha` on the `Psicologos` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Psicologos" DROP COLUMN "senha";
